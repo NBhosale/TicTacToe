8a476c6b99af2ced4701da3566918df26be3caea
Niranjan Bhosale <niranjanbhosale24@gmail.com>
2016-10-13 13:40:04 -0700

    Created new Activity and changed layout for playMode screen

---
+++ app/src/main/java/com/example/nero/tictactoe/PlayModeActivity.java
@@ -0,0 +1,14 @@
+package com.example.nero.tictactoe;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PlayModeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.playmode);
+    }
+}
--- app/src/main/java/com/example/nero/tictactoe/PlayScreen.java
+++ app/src/main/java/com/example/nero/tictactoe/PlayScreen.java
@@ -1,5 +1,6 @@
 package com.example.nero.tictactoe;
 
+import android.content.Intent;
 import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -11,27 +12,46 @@ import android.widget.ListView;
 
 public class PlayScreen extends AppCompatActivity implements View.OnClickListener{
 
-    private Button Exit;
+    private Button exit, play, about;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play_screen);
 
-        ListView listView;
-        AdapterView.OnItemClickListener itemListener = new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        play = (Button) findViewById(R.id.playButton);
+        play.setOnClickListener(this);
 
-            }
-        };
+        about = (Button) findViewById(R.id.aboutButton);
+        about.setOnClickListener(this);
 
-        Exit = (Button) findViewById(R.id.exitButton);
-        Exit.setOnClickListener(this);
+        exit = (Button) findViewById(R.id.exitButton);
+        exit.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
 
+        switch (v.getId()) {
+
+            case R.id.playButton:
+                Intent intent = new Intent(this, PlayModeActivity.class);
+                startActivity(intent);
+                break;
+
+            case R.id.aboutButton:
+
+                break;
+
+            case R.id.exitButton:
+                finish();
+                System.exit(0);
+                break;
+
+            default:
+                break;
+        }
+
     }
+
 }
